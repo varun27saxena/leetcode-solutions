bd045a4a917985e5e60405ce4c7c442112093a2e
varunXsensei <varun.saxena.dev27@gmail.com>
2026-07-14 18:53:59 +0530

    Time: 0 ms (100%), Space: 26.7 MB (26.92%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkPalindrome(int i, int j, string s) {
+        while (i <= j) {
+            if (s[i] != s[j])
+                return false;
+            i++, j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s[i] != s[j]) {
+                return checkPalindrome(i + 1, j, s) ||
+                       checkPalindrome(i, j - 1, s);
+            } else {
+                i++, j--;
+            }
+        }
+        return true;
+    }
+};
