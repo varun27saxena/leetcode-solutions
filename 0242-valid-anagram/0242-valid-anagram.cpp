03f6612cc34765bc847541f74bef009d76b494b1
varunXsensei <varun.saxena.dev27@gmail.com>
2026-07-16 09:18:16 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hash[26] = {0};
+        for(int i = 0;i<s.length();i++){
+            hash[s[i]-'a']++;
+        }
+
+        for(int i = 0;i<t.length();i++){
+            hash[t[i]-'a']--;
+        }
+        
+        bool flag = true;
+        for(int i = 0;i<26;i++){
+            if(hash[i] != 0)flag = false;
+        }
+        return flag;
+    }
+};
